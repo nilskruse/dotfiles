5ea1fcf7df31e58239ed18d0586c3496c8c30d9f
Nils Kruse <nils.kruse@stud.uni-due.de>
2022-04-12 18:57:40 +0200

    Update dot_config/nvim/init.lua Update dot_config/nvim/plugins.lua

--- dot_config/nvim/plugins.lua
+++ dot_config/nvim/plugins.lua
@@ -2,8 +2,9 @@ local Plug = vim.fn['plug#']
 
 vim.call('plug#begin', '~/.config/nvim/plugged')
 
-        Plug 'lervag/vimtex'
-        Plug 'neovim/nvim-lspconfig'
+    Plug 'lervag/vimtex'
+    Plug 'neovim/nvim-lspconfig'
+    Plug 'andweeb/presence.nvim'
 
 vim.call('plug#end')
 
