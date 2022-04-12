20738ef6ef403bc19f7e9a5393748720697d79b1
Nils Kruse <nils.kruse@stud.uni-due.de>
2022-04-12 18:31:36 +0200

    Update dot_config/nvim/init.lua

--- dot_config/nvim/init.lua
+++ dot_config/nvim/init.lua
@@ -7,11 +7,12 @@ vim.call('plug#begin', '~/.config/nvim/plugged')
         Plug 'neovim/nvim-lspconfig'
 
 vim.call('plug#end')
+    
 
 o.expandtab      = true
 o.autoindent     = true
 o.copyindent     = true
-o.smarttab       = true
+o.smarttab       = false
 o.tabstop        = 4
 o.softtabstop    = 4
 o.shiftwidth     = 4
