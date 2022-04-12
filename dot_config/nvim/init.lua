03a75e0b90883306f21ce24c58c08a06316412b6
Nils Kruse <nils.kruse@stud.uni-due.de>
2022-04-12 18:27:48 +0200

    Add dot_config/nvim/init.lua

---
+++ dot_config/nvim/init.lua
@@ -0,0 +1,17 @@
+local Plug = vim.fn['plug#']
+local o = vim.opt
+
+vim.call('plug#begin', '~/.config/nvim/plugged')
+
+        Plug 'lervag/vimtex'
+        Plug 'neovim/nvim-lspconfig'
+
+vim.call('plug#end')
+
+
+o.expandtab      = true
+o.autoindent     = true
+o.copyindent     = true
+o.tabstop        = 4
+o.softtabstop    = 4
+o.shiftwidth     = 4
