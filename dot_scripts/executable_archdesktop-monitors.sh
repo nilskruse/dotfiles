5360f6dbff6d0c2aa0c918b6102b535e84843c93
Nils Kruse <nils.kruse@stud.uni-due.de>
2022-04-14 16:51:08 +0200

    Add dot_scripts/executable_archdesktop-monitors.sh Add dot_scripts/kde-bspwm.sh

---
+++ dot_scripts/executable_archdesktop-monitors.sh
@@ -0,0 +1,2 @@
+#! /bin/sh
+xrandr --output DVI-D-0 --mode 1920x1080 --rate 144.00 --left-of DP-0 --output DP-0 --primary --mode 1920x1080 --rate 144.00 --output DP-3 --mode 1920x1080 --rate 60.00 --right-of DP-0
