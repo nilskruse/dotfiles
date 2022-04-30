d11f76e02617b69618a8d5b4defdeedf2adf0622
Nils Kruse <nils.kruse@stud.uni-due.de>
2022-04-30 18:02:46 +0200

    Update dot_config/bspwm/executable_bspwmrc Update dot_config/nvim/lsp-keymaps.lua Update dot_config/nvim/plugins.lua

--- dot_config/nvim/plugins.lua
+++ dot_config/nvim/plugins.lua
@@ -6,7 +6,10 @@ vim.call('plug#begin', '~/.config/nvim/plugged')
     Plug 'neovim/nvim-lspconfig'
     Plug 'andweeb/presence.nvim'
     Plug 'morhetz/gruvbox'
-
+    Plug 'hrsh7th/nvim-cmp' -- Autocompletion plugin
+    Plug 'hrsh7th/cmp-nvim-lsp' -- LSP source for nvim-cmp
+    Plug 'saadparwaiz1/cmp_luasnip' -- Snippets source for nvim-cmp
+    Plug 'L3MON4D3/LuaSnip' -- Snippets plugin
 vim.call('plug#end')
 
 
