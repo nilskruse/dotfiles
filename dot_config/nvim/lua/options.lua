8ab520ee3f1058b189e1ac0ffd49ec0a60adec1b
Nils Kruse <nils.kruse@stud.uni-due.de>
2022-05-03 16:49:24 +0200

    Add dot_config/nvim/lua/lsp-keymaps.lua Add dot_config/nvim/lua/options.lua Add dot_config/nvim/lua/plugins.lua

---
+++ dot_config/nvim/lua/options.lua
@@ -0,0 +1,13 @@
+local o = vim.opt
+
+o.expandtab      = true
+o.autoindent     = true
+o.copyindent     = true
+o.smarttab       = true
+o.tabstop        = 4
+o.softtabstop    = 4
+o.shiftwidth     = 4
+o.relativenumber = true
+o.cursorline     = true
+o.scrolloff      = 8
+o.termguicolors  = true
