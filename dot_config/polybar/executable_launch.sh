cb693707e23a86701eea37252051437ff5cb4398
Nils Kruse <nils.kruse@stud.uni-due.de>
2022-05-11 16:35:05 +0200

    Update dot_config/bspwm/executable_bspwmrc Update dot_config/polybar/executable_launch.sh Update dot_zprofile Update dot_zshrc

--- dot_config/polybar/executable_launch.sh
+++ dot_config/polybar/executable_launch.sh
@@ -2,8 +2,11 @@
 
 # Terminate already running bar instances
 killall -q polybar
+
 CONFIGFILE=$1
 BAR=$2
+TRAYMONITOR="DP-0"
+
 if [ -z "$1" ]
   then
     CONFIGFILE=$XDG_CONFIG_HOME/polybar/config.ini
@@ -19,7 +22,7 @@ while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 if [[ "$(hostname)" == "archdesktop" ]]; then
     if type "xrandr"; then
       for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
-        if [ "$m" == "DP-0" ]; then
+        if [ "$m" == $TRAYMONITOR ]; then
           TRAYPOS="right" MONITOR=$m polybar --reload $BAR --config=$CONFIGFILE &
         else
           MONITOR=$m polybar --reload $BAR --config=$CONFIGFILE &
