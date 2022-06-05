3aee687350a3800ca82b02fa38c78036e0974dde
Nils Kruse <nils.kruse@stud.uni-due.de>
2022-06-05 21:55:14 +0200

    Update dot_config/bspwm/executable_bspwmrc Update dot_config/nvim/lua/lsp-keymaps.lua Update dot_zprofile

--- dot_config/nvim/lua/lsp-keymaps.lua
+++ dot_config/nvim/lua/lsp-keymaps.lua
@@ -1,5 +1,6 @@
 require'lspconfig'.clangd.setup{}
 require'lspconfig'.jedi_language_server.setup{}
+-- require'lspconfig'.pyright.setup{}
 
 -- Add additional capabilities supported by nvim-cmp
 local capabilities = vim.lsp.protocol.make_client_capabilities()
